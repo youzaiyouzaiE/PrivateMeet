579c1c8e0be9ec3d769ac8b02aee0ad06abb24cd
Jiahui <goodzhoujiahui@gmail.com>
2016-05-14 14:11:58 +0800

    add send invite file

---
+++ Meet/Meet/ViewControllers/Me/My Invite/SendInviteViewController.h
@@ -0,0 +1,15 @@
+//
+//  SendInviteViewController.h
+//  Meet
+//
+//  Created by jiahui on 16/5/14.
+//  Copyright © 2016年 Meet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendInviteViewController : UIViewController
+
+
+
+@end
---
+++ Meet/Meet/ViewControllers/Me/My Invite/SendInviteViewController.m
@@ -0,0 +1,13 @@
+//
+//  SendInviteViewController.m
+//  Meet
+//
+//  Created by jiahui on 16/5/14.
+//  Copyright © 2016年 Meet. All rights reserved.
+//
+
+#import "SendInviteViewController.h"
+
+@implementation SendInviteViewController
+
+@end
